2f513e6c2ee54a87dbb84c55edf18e1e75003948
ocasek <necas.marty@gmail.com>
2018-03-26 15:12:46 +0200

    innit db

--- web/main/models.py
+++ web/main/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=30, default="")
+    password = models.CharField(max_length=300, default="")
+    email = models.EmailField(default="")
+
+    def right_user(self, username, password):
+        if username == self.username and self.password == password:
+            return True
+        return False
+
+    def is_registred(self, username):
+        if username == self.username:
+            return True
+        return False
+
+    def __str__(self):
+        return str(self.username)
+
+class Event(models.Model):
+    name = models.CharField(max_length=30, default="")
+    text = models.CharField(max_length=3000, default="")
+
+    def __str__(self):
+        return str(self.name)
+
+class Image(models.Model):
+    image = models.ImageField(default="")
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
