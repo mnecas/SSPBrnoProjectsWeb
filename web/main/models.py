7460e21248777be70d1c79e2f1a7da08f82b9557
ocasek <necas.marty@gmail.com>
2018-03-28 13:39:06 +0200

    update db

--- web/main/models.py
+++ web/main/models.py
@@ -3,11 +3,30 @@ from django.core.files.storage import FileSystemStorage
 from django.conf import settings
 
 
+
+
+
+class Event(models.Model):
+    name = models.CharField(max_length=30, default="")
+    text = models.TextField(max_length=3000, default="")
+    is_admin = models.BooleanField(default=False)
+    def get_images(self):
+        return Image.objects.filter(event=self)
+
+    def __str__(self):
+        return str(self.name)
+
+
+class Image(models.Model):
+    image = models.ImageField()
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+
+
 class User(models.Model):
     username = models.CharField(max_length=30, default="")
     password = models.CharField(max_length=300, default="")
     email = models.EmailField(default="")
-
+    icon = models.ForeignKey(Image, on_delete=models.CASCADE,default=None)
     def right_user(self, username, password):
         if username == self.username and self.password == password:
             return True
@@ -21,18 +40,12 @@ class User(models.Model):
     def __str__(self):
         return str(self.username)
 
+class Comment(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE,default=None)
+    user = models.ForeignKey(User, on_delete=models.CASCADE,default=None)
+    text = models.TextField(max_length=300,default="")
 
-class Event(models.Model):
-    name = models.CharField(max_length=30, default="")
-    text = models.TextField(max_length=3000, default="")
-
-    def get_images(self):
-        return Image.objects.filter(event=self)
-
-    def __str__(self):
-        return str(self.name)
-
-
-class Image(models.Model):
-    image = models.ImageField()
-    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+class Anketa(models.Model):
+    question = models.CharField(max_length=30, default="")
+    event = models.ForeignKey(Event, on_delete=models.CASCADE,default=None)
+    #points =
