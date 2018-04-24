7e69e4401d65998bdc60a0c341002d73ab2cfd80
wudiam <wudiamik@gmail.com>
2018-04-24 12:06:25 +0200

    Still doesnt work

--- web/static/js/script.js
+++ web/static/js/script.js
@@ -1,5 +1,6 @@
-var elem = document.querySelector('.sidenav');
-var instance = M.Sidenav.init(elem, options);
+$(document).ready(function(){
+   $('.sidenav').sidenav();
+});
 
 $(document).ready(function(){
   $('.sidenav').sidenav();
