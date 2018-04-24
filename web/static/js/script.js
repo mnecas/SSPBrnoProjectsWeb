cfe4e65facc519a33ddc06032118bd1fb7890be8
wudiam <wudiamik@gmail.com>
2018-04-24 10:32:10 +0200

    Trying to fix navbar on mobile

--- web/static/js/script.js
+++ web/static/js/script.js
@@ -1,3 +1,10 @@
+var elem = document.querySelector('.sidenav');
+var instance = M.Sidenav.init(elem, options);
+
+$(document).ready(function(){
+  $('.sidenav').sidenav();
+});
+
 $(document).ready(function () {
     $('input#input_text, textarea#textarea2').characterCounter();
 });
